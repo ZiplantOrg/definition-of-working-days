867b8183882a9d0fbaab9858cf79c49bb278d51a
vladislav ziplant <v.sinitsyn@open-bs.ru>
2025-08-26 17:09:51 +0300

    сдлела istance date, нужно сделать duration

--- parsing_taxonomy/read_taxonomy.py
+++ parsing_taxonomy/read_taxonomy.py
@@ -23,9 +23,14 @@ async def read_taxonomy():
             base_dir = os.path.abspath(folder_path)  # получаем абсолютный путь к 'taxonomy'
             # проверяем каждый рынок из переменной market_segments
             for segment in market_segments:
-               # Получаем список точек входа
-               file_names  = await entry_point(segment,base_dir,path_market)
-               print("Файлы в папке 'ep' (без расширения):", file_names)
+
+               full_segment_path = os.path.join(base_dir, path_market, segment)
+               # Получаем список точек входа и их компоненты по формам
+               entry_points_forms  = await entry_point(full_segment_path)
+               # Получаем список точек входа и форм + периоды
+               entry_points_forms_period = await get_period_from_rend(entry_points_forms,full_segment_path)
+
+               print("Файлы в папке 'ep' (без расширения):", entry_points_forms)
 
 
         else:
@@ -39,8 +44,7 @@ async def read_taxonomy():
         print(f"Ошибка произошла:\n{tb}")
 
 # Функция перебора точек входа
-async def entry_point(segment, base_dir, path_market):
-    full_segment_path = os.path.join(base_dir, path_market, segment)
+async def entry_point(full_segment_path):
     entry_point = {}
     period_values_by_file = {}  # Новый словарь для хранения значений из -rend.xml
 
@@ -48,6 +52,7 @@ async def entry_point(segment, base_dir, path_market):
         for root, dirs, files in os.walk(full_segment_path):
             if "ep" in os.path.basename(root).lower():
                 for f in files:
+
                     file_path = os.path.join(root, f)
                     if f.endswith('.xsd'):
                         try:
@@ -67,83 +72,87 @@ async def entry_point(segment, base_dir, path_market):
                             tab_paths = [path for path in schema_locations if path.startswith('../tab')]
                             entry_point[f] = tab_paths  # Сохраняем отдельно
 
-                            for root2, dirs, files2 in os.walk(full_segment_path):
-                                if "tab" in os.path.basename(root2).lower():
-                                    path_tab = root2.removesuffix("tab")
-                                    # Вызов новой функции для получения значения из -rend.xml
-                                    period_value = await get_period_from_rend(path_tab, entry_point)
-                                    period_values_by_file[f] = period_value
+                            # for root2, dirs, files2 in os.walk(full_segment_path):
+                            #     if "tab" in os.path.basename(root2).lower():
+                            #         path_tab = root2.removesuffix("tab")
+                            #         # Вызов новой функции для получения значения из -rend.xml
+                            #         period_value = await get_period_from_rend(path_tab, entry_point)
+                            #         period_values_by_file[f] = period_value
 
                         except ET.ParseError:
                             print(f"Ошибка парсинга XML: {file_path}")
+        return entry_point
 
-        return entry_point, period_values_by_file
     else:
-        print(f"Папка '{segment}' не существует: {full_segment_path}")
+        print(f"Папка '' не существует: {full_segment_path}")
         return {}, {}
 
 
 # функция получения всех ref period в рамках одной точки входа
-async def get_period_from_rend(path_tab, entry_point):
-    for key, value_list in entry_point.items():
-        print(f"Ключ: {key}")
-        for value in value_list:
-            clean_value = value.replace('../', '', 1)
-            rend_path = path_tab + '/' + clean_value
+async def get_period_from_rend(entry_points_forms,full_segment_path):
+    if os.path.exists(full_segment_path) and os.path.isdir(full_segment_path):
+        for root, dirs, files in os.walk(full_segment_path):
+            if "tab" in os.path.basename(root).lower():
+                path_tab = root.removesuffix("tab")
+                for key_entry_point, value_list in entry_points_forms.items():
+                    print(f"Ключ: {key_entry_point}")
+                    for value in value_list:
+                        clean_value = value.replace('../', '', 1)
+                        rend_path = path_tab + '/' + clean_value
 
-            if not os.path.exists(rend_path):
-                return None
+                        if not os.path.exists(rend_path):
+                            return None
+                        try:
+                            tree = ET.parse(rend_path)
+                            root = tree.getroot()
 
-            try:
-                tree = ET.parse(rend_path)
-                root = tree.getroot()
+                            # Определение пространств имён (важно для корректной работы XPath)
+                            namespaces = {
+                                'xsd': 'http://www.w3.org/2001/XMLSchema',
+                                'link': 'http://www.xbrl.org/2003/linkbase'
+                            }
 
-                # Определение пространств имён (важно для корректной работы XPath)
-                namespaces = {
-                    'xsd': 'http://www.w3.org/2001/XMLSchema',
-                    'link': 'http://www.xbrl.org/2003/linkbase'
-                }
+                            # XPath запрос для получения всех link:linkbaseRef
+                            xpath_query = '//xsd:annotation/xsd:appinfo/link:linkbaseRef'
 
-                # XPath запрос для получения всех link:linkbaseRef
-                xpath_query = '//xsd:annotation/xsd:appinfo/link:linkbaseRef'
+                            # Выполнение запроса
+                            linkbase_refs = root.xpath(xpath_query, namespaces=namespaces)
 
-                # Выполнение запроса
-                linkbase_refs = root.xpath(xpath_query, namespaces=namespaces)
+                            # Список для хранения абсолютных путей
+                            absolute_paths = []
 
-                # Список для хранения абсолютных путей
-                absolute_paths = []
+                            # перебор форм rend внутри уже формы.
+                            for ref in linkbase_refs:
+                                href = ref.get('{http://www.w3.org/1999/xlink}href')
+                                if href:
+                                    # Объединяем путь до xsd-файла и имя файла из xlink:href
 
-                # перебор форм rend внутри уже формы.
-                for ref in linkbase_refs:
-                    href = ref.get('{http://www.w3.org/1999/xlink}href')
-                    if href:
-                        # Объединяем путь до xsd-файла и имя файла из xlink:href
+                                    # Преобразуем строку в объект Path
+                                    path = Path(rend_path)
+                                    # Переходим на один уровень выше
+                                    parent_path = path.parent
 
-                        # Преобразуем строку в объект Path
-                        path = Path(rend_path)
-                        # Переходим на один уровень выше
-                        parent_path = path.parent
+                                    full_path = os.path.join(parent_path, href)
+                                    absolute_paths.append(full_path)
 
-                        full_path = os.path.join(parent_path, href)
-                        absolute_paths.append(full_path)
+                                    formula_instant_values = await formula_instant(key_entry_point,absolute_paths, entry_points_forms)
 
-                        formula_instant_values = await formula_instant(absolute_paths, entry_point)
+                                    if 'new_object' not in locals():
+                                        new_object = formula_instant_values  # Первый запуск: присвоение значения
+                                    else:
+                                        if formula_instant_values != new_object:
+                                            new_object.update(formula_instant_values)
+                                            print("Значения отличаются")
+                            print(new_object)
 
-                        if 'new_object' not in locals():
-                            new_object = formula_instant_values  # Первый запуск: присвоение значения
-                        else:
-                            if formula_instant_values != new_object:
-                                new_object.update(formula_instant_values)
-                                print("Значения отличаются")
-                print(new_object)
-
-            except ET.ParseError as e:
-                print(f"Ошибка парсинга XML в файле {rend_path}: {e}")
-        return new_object
+                        except ET.ParseError as e:
+                            print(f"Ошибка парсинга XML в файле {rend_path}: {e}")
+                return new_object
 
 
 # обработка  <formula:period> <formula:instant value="$par:refPeriodEnd"/>  </formula:period>
-async def formula_instant (absolute_paths,entry_point):
+async def formula_instant (key_entry_point, absolute_paths,entry_points_forms):
+    key_entry_point = key_entry_point
     values = []
     # Обрабатываем каждый файл из absolute_paths
     for file_path in absolute_paths:
@@ -160,15 +169,16 @@ async def formula_instant (absolute_paths,entry_point):
                     if value:
                         if not values or value not in values:  # Проверка на пустоту и дублирование
                             values.append(value)
-                            entry_name = next(iter(entry_point.keys()))
+                            # entry_name = next(iter(entry_points_forms.keys()))
+                            entry_name = key_entry_point
                             # Если entry_point[entry_name] ещё не является словарём, преобразуем его
-                            if not isinstance(entry_point[entry_name], dict):
-                                entry_point[entry_name] = {}
+                            if not isinstance(entry_points_forms[entry_name], dict):
+                                entry_points_forms[entry_name] = {}
                             # Используем значение переменной key как ключ в словаре
                             key = value
-                            entry_point[entry_name][key] = value
+                            entry_points_forms[entry_name][key] = value
 
         except Exception as e:
             print(f"Ошибка при обработке файла {file_path}: {e}")
-    return entry_point
+    return entry_points_forms
 
