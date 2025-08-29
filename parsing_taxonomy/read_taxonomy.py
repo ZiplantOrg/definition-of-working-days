2be38485b3a839c1735cd92b901de76ba9c394f4
vladislav ziplant <v.sinitsyn@open-bs.ru>
2025-08-29 12:33:43 +0300

    собрали все точки входа по рынку uk и sd

--- parsing_taxonomy/read_taxonomy.py
+++ parsing_taxonomy/read_taxonomy.py
@@ -14,24 +14,35 @@ async def read_taxonomy():
     try:
         # Проверяем, существует ли папка taxonomy
         if os.path.exists(folder_path) and os.path.isdir(folder_path):
+
             # Получаем список всех подпапок
             subfolders = [f.name for f in os.scandir(folder_path) if f.is_dir()]
-            # print("Подпапки в 'taxonomy':", subfolders)
 
             # получаем путь к папке nso
             path_market = f'{subfolders[0]}/{path_market}'  # без лишнего слэша в начале
-            base_dir = os.path.abspath(folder_path)  # получаем абсолютный путь к 'taxonomy'
-            # проверяем каждый рынок из переменной market_segments
-            for segment in market_segments:
 
+            # получаем абсолютный путь к 'taxonomy'
+            base_dir = os.path.abspath(folder_path)
+
+            # Инициализация пустого словаря
+            entry_points_all_segment_forms_period = {}
+
+            # прорабатываем каждый рынок из переменной market_segments присваимем периоды которые находятся в каждой точке входа
+            for segment in market_segments:
+               # получаем путь к сегменту рынка
                full_segment_path = os.path.join(base_dir, path_market, segment)
+
                # Получаем список точек входа и их компоненты по формам
                entry_points_forms  = await entry_point(full_segment_path)
+
                # Получаем список точек входа и форм + периоды
-               entry_points_forms_period = await get_period_from_rend(entry_points_forms,full_segment_path)
+               entry_points_one_segment_forms_period = await get_period_from_rend(entry_points_forms,full_segment_path)
+               # Обновляем список с точками входа
+               entry_points_all_segment_forms_period.update(entry_points_one_segment_forms_period)
 
-               print("Файлы в папке 'ep' (без расширения):", entry_points_forms)
-
+            # присвоить маркеры month_obj/quart_obj/year_obj
+            entry_points_all_segment_forms_period =  await period_marker(entry_points_all_segment_forms_period)
+            print("точка входа период:", entry_points_all_segment_forms_period)
 
         else:
             print(f"Папка '{folder_path}' не существует.")
@@ -45,14 +56,14 @@ async def read_taxonomy():
 
 # Функция перебора точек входа
 async def entry_point(full_segment_path):
+    # Инцилизация списка
     entry_point = {}
-    period_values_by_file = {}  # Новый словарь для хранения значений из -rend.xml
 
     if os.path.exists(full_segment_path) and os.path.isdir(full_segment_path):
         for root, dirs, files in os.walk(full_segment_path):
             if "ep" in os.path.basename(root).lower():
                 for f in files:
-
+                    # получаем список точек входа в сегменте рынка
                     file_path = os.path.join(root, f)
                     if f.endswith('.xsd'):
                         try:
@@ -66,27 +77,19 @@ async def entry_point(full_segment_path):
                                 )
                                 if elem.get("schemaLocation")
                             ]
-
                             entry_point[f] = schema_locations
                             # Добавляем фильтрацию и сохранение только путей, начинающихся с '../tab'
                             tab_paths = [path for path in schema_locations if path.startswith('../tab')]
                             entry_point[f] = tab_paths  # Сохраняем отдельно
 
-                            # for root2, dirs, files2 in os.walk(full_segment_path):
-                            #     if "tab" in os.path.basename(root2).lower():
-                            #         path_tab = root2.removesuffix("tab")
-                            #         # Вызов новой функции для получения значения из -rend.xml
-                            #         period_value = await get_period_from_rend(path_tab, entry_point)
-                            #         period_values_by_file[f] = period_value
-
                         except ET.ParseError:
-                            print(f"Ошибка парсинга XML: {file_path}")
+                            print(f"Ошибка парсинга XML entry_point : {file_path}")
+
         return entry_point
 
     else:
         print(f"Папка '' не существует: {full_segment_path}")
         return {}, {}
-
 
 # функция получения всех ref period в рамках одной точки входа
 async def get_period_from_rend(entry_points_forms,full_segment_path):
@@ -135,6 +138,8 @@ async def get_period_from_rend(entry_points_forms,full_segment_path):
                                     full_path = os.path.join(parent_path, href)
                                     absolute_paths.append(full_path)
 
+                                    absolute_paths = [path for path in absolute_paths if path.endswith('-rend.xml')]
+                                    # обрабатываем даты instance
                                     formula_instant_values = await formula_instant(key_entry_point,absolute_paths, entry_points_forms)
 
                                     if 'new_object' not in locals():
@@ -143,16 +148,23 @@ async def get_period_from_rend(entry_points_forms,full_segment_path):
                                         if formula_instant_values != new_object:
                                             new_object.update(formula_instant_values)
                                             print("Значения отличаются")
-                            print(new_object)
+
+                                    # обрабатываем даты duration
+                                    formula_duration_values = await formula_duration(key_entry_point, absolute_paths, entry_points_forms)
+
+                                    if 'new_object' not in locals():
+                                        new_object = formula_duration_values  # Первый запуск: присвоение значения
+                                    else:
+                                        if formula_duration_values != new_object:
+                                            new_object.update(formula_duration_values)
+                                            print("Значения отличаются")
 
                         except ET.ParseError as e:
                             print(f"Ошибка парсинга XML в файле {rend_path}: {e}")
                 return new_object
 
-
 # обработка  <formula:period> <formula:instant value="$par:refPeriodEnd"/>  </formula:period>
 async def formula_instant (key_entry_point, absolute_paths,entry_points_forms):
-    key_entry_point = key_entry_point
     values = []
     # Обрабатываем каждый файл из absolute_paths
     for file_path in absolute_paths:
@@ -171,6 +183,43 @@ async def formula_instant (key_entry_point, absolute_paths,entry_points_forms):
                             values.append(value)
                             # entry_name = next(iter(entry_points_forms.keys()))
                             entry_name = key_entry_point
+                            # Если entry_points_forms[entry_name] ещё не является словарём, преобразуем его
+                            if not isinstance(entry_points_forms[entry_name], dict):
+                                entry_points_forms[entry_name] = {}
+                            # Используем значение переменной key как ключ в словаре
+                            key = value
+                            entry_points_forms[entry_name][key] = value
+
+
+        except Exception as e:
+            print(f"Ошибка при обработке файла {file_path}: {e}")
+    return entry_points_forms
+
+# обработка  <formula:period> <formula:duration
+async def formula_duration (key_entry_point, absolute_paths,entry_points_forms):
+    key_entry_point = key_entry_point
+    values = []
+    # Обрабатываем каждый файл из absolute_paths
+    for file_path in absolute_paths:
+        try:
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+            instants = root.xpath("//*[local-name()='link']/*[local-name()='ruleNode']/*[local-name()='period']")
+            # Поиск всех элементов <formula:period>
+            for period_element in instants:
+                # Внутри каждого <formula:period> ищем <formula:instant>
+                instant_element = period_element.xpath("./*[local-name()='duration']")
+                for instant in instant_element:
+                    value = instant.get('start')
+                    if '+' in value:
+                        # Удаляем все, что идет после символа '+'
+                        value = value.split('+')[0].strip()
+
+                    if value:
+                        if not values or value not in values:  # Проверка на пустоту и дублирование
+                            values.append(value)
+                            # entry_name = next(iter(entry_points_forms.keys()))
+                            entry_name = key_entry_point
                             # Если entry_point[entry_name] ещё не является словарём, преобразуем его
                             if not isinstance(entry_points_forms[entry_name], dict):
                                 entry_points_forms[entry_name] = {}
@@ -182,3 +231,48 @@ async def formula_instant (key_entry_point, absolute_paths,entry_points_forms):
             print(f"Ошибка при обработке файла {file_path}: {e}")
     return entry_points_forms
 
+async def period_marker(entry_points_all_segment_forms_period):
+    # Создаем копию ключей, чтобы избежать изменения словаря во время итерации
+    for entry_point in list(entry_points_all_segment_forms_period.keys()):
+        key_period = "key_period"
+
+        if "m" in entry_point and "q" not in entry_point and "y" not in entry_point:
+            key = "month_obj"
+            entry_points_all_segment_forms_period[entry_point][key_period] = key
+
+        elif "q" in entry_point and "m" not in entry_point and "y" not in entry_point:
+            key = "quart_obj"
+            entry_points_all_segment_forms_period[entry_point][key_period] = key
+
+        elif "y" in entry_point and "m" not in entry_point and "q" not in entry_point:
+            key = "year_obj"
+            entry_points_all_segment_forms_period[entry_point][key_period] = key
+
+        elif "m_q_y" in entry_point:
+            original = entry_points_all_segment_forms_period[entry_point].copy()
+
+            # Дубликат 1: year_obj
+            original_copy_1 = original.copy()
+            original_copy_1[key_period] = "year_obj"
+            entry_points_all_segment_forms_period[f"{entry_point}_year"] = original_copy_1
+
+            # Дубликат 2: quart_obj
+            original_copy_2 = original.copy()
+            original_copy_2[key_period] = "quart_obj"
+            entry_points_all_segment_forms_period[f"{entry_point}_quart"] = original_copy_2
+
+            # Дубликат 3: month_obj
+            original_copy_3 = original.copy()
+            original_copy_3[key_period] = "month_obj"
+            entry_points_all_segment_forms_period[f"{entry_point}_month"] = original_copy_3
+
+            # Удаляем оригинальный элемент
+            del entry_points_all_segment_forms_period[entry_point]
+        else:
+            key = "null"
+            entry_points_all_segment_forms_period[entry_point][key_period] = key
+
+    return entry_points_all_segment_forms_period
+
+
+        # entry_points_forms[entry_point][key] = value
